2ee978f353397f6c06e1766530abc524baadcd64
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-09-25 15:38:04 -0300

    better design and correct redirect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-const projects = document.getElementsByClassName(im);
+function redirect($url) {
+    switch ($url) {
+        case "rickandmorty":
+            window.open('http://www.leonelmoyano.com/RickAndMortyAPI/', '_blank');
+            break;
+        case "controlpanel":
+            window.open('http://www.leonelmoyano.com/controlpanel/cargar_pedidos.php', '_blank');
+            break;
+    }
+}
