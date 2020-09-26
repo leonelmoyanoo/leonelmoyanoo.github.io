0918240a3f10f142332c2c2223d472a38e8ccc68
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-09-26 19:24:48 -0300

    some effects :D

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    
+document.addEventListener("DOMContentLoaded", function (event) {
+    let mePic = document.getElementsByClassName("resume__image");
+    let description = document.getElementsByClassName("resume");
+    let body = document.getElementsByClassName("down-body");
+
+
+    description[0].classList.remove("up-title");
+    description[0].classList.add("down-title");
+
+    console.table(body[0]);
+    console.log(body[1]);
+
+    body[0].classList.add("up-body");
+    body[1].classList.add("up-body");
+    body[1].classList.remove("down-body");
+    body[0].classList.remove("down-body");
+
+    setTimeout(()=>{
+        mePic[0].classList.add("rotate");
+    },1100)
 });
 function redirect($url) {
     switch ($url) {
