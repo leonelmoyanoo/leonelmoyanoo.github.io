44cbb5a82fb3fcb68e5f9ab7c48e1878f6dea90e
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-10-24 17:08:44 -0300

    adding fontawesome folder

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let mePic = document.getElementsByClassName("resume__image");
-    let description = document.getElementsByClassName("up-title");
-    let body = document.getElementsByClassName("down-body");
-
-    body[0].classList.add("up-body");
-    body[1].classList.add("up-body");
-    body[1].classList.remove("down-body");
-    body[0].classList.remove("down-body");
 
     setTimeout(()=>{
         mePic[0].classList.add("rotate");
-    },1100)
+    },500)
 });
