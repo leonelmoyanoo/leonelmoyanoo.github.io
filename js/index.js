db28a7f23b1d1e955780cc40c6a733cb869a6fbe
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-10-24 19:40:35 -0300

    new navbar responsive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    let mePic = document.getElementsByClassName("resume__image");
 
-    setTimeout(()=>{
+var navBar = document.getElementsByClassName("header__nav")[0];
+document.addEventListener("DOMContentLoaded", function (event) {
+    var mePic = document.getElementsByClassName("resume__image");
+
+    setTimeout(() => {
         mePic[0].classList.add("rotate");
-    },500)
+    }, 500)
+
+
 });
+function subMenu() {
+    if (navBar.className === "header__nav") {
+        navBar.className += " responsive";
+    } else {
+        navBar.className = "header__nav";
+    }
+}
+function redirectOnTheSamePage(){
+    if (navBar.className === "header__nav responsive") {
+        navBar.className = "header__nav";
+    }
+}
