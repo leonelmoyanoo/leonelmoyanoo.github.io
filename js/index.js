1d3259d797def674cb2e01526d6865045aada57b
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-10-24 03:01:30 -0300

    better header

--- js/index.js
+++ js/index.js
@@ -2,20 +2,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
     let mePic = document.getElementsByClassName("resume__image");
     let description = document.getElementsByClassName("up-title");
     let body = document.getElementsByClassName("down-body");
-    console.log(description);
-
-
-    description[1].classList.add("down-title");
-    description[1].classList.remove("up-title");
 
     body[0].classList.add("up-body");
     body[1].classList.add("up-body");
     body[1].classList.remove("down-body");
     body[0].classList.remove("down-body");
-
-
-    description[0].classList.add("container");
-    description[0].classList.remove("up-title");
 
     setTimeout(()=>{
         mePic[0].classList.add("rotate");
