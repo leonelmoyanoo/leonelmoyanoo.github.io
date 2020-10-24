b748f26dfb084cd41bd7c74cf27925d426e95311
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-10-24 02:04:13 -0300

    links to redirect on the same page

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let mePic = document.getElementsByClassName("resume__image");
-    let description = document.getElementsByClassName("resume");
+    let description = document.getElementsByClassName("up-title");
     let body = document.getElementsByClassName("down-body");
+    console.log(description);
 
 
-    description[0].classList.remove("up-title");
-    description[0].classList.add("down-title");
+    description[1].classList.add("down-title");
+    description[1].classList.remove("up-title");
 
     body[0].classList.add("up-body");
     body[1].classList.add("up-body");
     body[1].classList.remove("down-body");
     body[0].classList.remove("down-body");
+
+
+    description[0].classList.add("container");
+    description[0].classList.remove("up-title");
 
     setTimeout(()=>{
         mePic[0].classList.add("rotate");
