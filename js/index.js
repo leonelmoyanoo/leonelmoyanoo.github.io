bfd438a391a9678ae02924b7ee78849a748b0bd4
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-10-23 21:45:55 -0300

    new colors and better images

--- js/index.js
+++ js/index.js
@@ -7,9 +7,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     description[0].classList.remove("up-title");
     description[0].classList.add("down-title");
 
-    console.table(body[0]);
-    console.log(body[1]);
-
     body[0].classList.add("up-body");
     body[1].classList.add("up-body");
     body[1].classList.remove("down-body");
@@ -19,13 +16,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
         mePic[0].classList.add("rotate");
     },1100)
 });
-function redirect($url) {
-    switch ($url) {
-        case "rickandmorty":
-            window.open('http://www.leonelmoyano.com/RickAndMortyAPI/', '_blank');
-            break;
-        case "controlpanel":
-            window.open('http://www.leonelmoyano.com/controlpanel/cargar_pedidos.php', '_blank');
-            break;
-    }
-}
